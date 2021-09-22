b2ef41bc770884764721ad95ac66a188a29b02e8
dwiana <dwiana.kamila.tif18@polban.ac.id>
2021-09-22 21:12:04 +0700

    Project Basic Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+<div id="nav">
+  <router-link to="/">Home</router-link>
+  <router-link :to="{ name: 'About' }">About</router-link>
+  <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
+</div>
+
+<button @click="redirect">Redirect</button>
+<button @click="back">Go Back</button>
+<button @click="forward">Go Forward</button>
+
+<router-view/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home' })
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    } 
+  }
+}
 </script>
+
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: center; 
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color:#2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+#nav a.router-link-exact-active {
+  color: bisque;
+  background: crimson;
+  }
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="home">
+        <h1>Homepage</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vel eligendi voluptatem eos voluptas, molestias, autem accusantium saepe perspiciatis ab at aut mollitia impedit itaque eveniet quibusdam aliquam. Qui vitae aspernatur voluptatem totam earum harum quas assumenda accusantium deleniti saepe, sint laborum dolorum amet numquam libero doloribus architecto nam eligendi esse maiores. Iure esse nihil quo quidem aliquam harum nemo nulla corrupti maxime ullam aspernatur maiores quas aperiam sunt, aliquid perferendis consectetur dicta ipsam asperiores accusantium. Sunt eos ullam rerum explicabo voluptates doloribus placeat magnam. Culpa neque rem obcaecati nisi eveniet similique? Laboriosam ipsum et exercitationem totam? Facilis, tempora laborum!</p> 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vel eligendi voluptatem eos voluptas, molestias, autem accusantium saepe perspiciatis ab at aut mollitia impedit itaque eveniet quibusdam aliquam. Qui vitae aspernatur voluptatem totam earum harum quas assumenda accusantium deleniti saepe, sint laborum dolorum amet numquam libero doloribus architecto nam eligendi esse maiores. Iure esse nihil quo quidem aliquam harum nemo nulla corrupti maxime ullam aspernatur maiores quas aperiam sunt, aliquid perferendis consectetur dicta ipsam asperiores accusantium. Sunt eos ullam rerum explicabo voluptates doloribus placeat magnam. Culpa neque rem obcaecati nisi eveniet similique? Laboriosam ipsum et exercitationem totam? Facilis, tempora laborum!</p>   
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    components: {  }
+}
+</script>
